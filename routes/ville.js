84c83d47622d3242aa3c948ad54f8e1f336ed38f
corentinravet <corentin.ravet@charioo.com>
2020-06-30 18:11:31 +0200

    :lipstick: (Adding Exercice 3)

--- routes/ville.js
+++ routes/ville.js
@@ -1,9 +1,31 @@
 var express = require("express");
 var router = express.Router();
+const axios = require("axios");
 
-/* GET home page. */
-router.post("/", function (req, res, next) {
-  res.render("ville", { nom_ville: req.body.nom_ville });
+/* GET Ville page. */
+router.get("/", function (req, res, next) {
+  res.render("ville");
+});
+
+router.post("/", async (req, res, next) => {
+  const nom_ville = req.body.nom_ville;
+
+  await axios
+    .get(`https://geocode.xyz/${nom_ville}?json=1`)
+    .then((response) => {
+      let longt = response.data.longt;
+      let latt = response.data.latt;
+
+      res.render("ville", { ville: nom_ville, latt: latt, longt: longt });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.render("ville", {
+        ville: nom_ville,
+        latt: "latitude not found",
+        longt: "longitude not found",
+      });
+    });
 });
 
 module.exports = router;
